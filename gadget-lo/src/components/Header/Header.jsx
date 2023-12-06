5fe428d89b43f41c7716672fc680106461239a2d
Deep Patil <59699631+patildeep07@users.noreply.github.com>
2023-12-06 22:47:22 +0530

    Header styling

--- gadget-lo/src/components/Header/Header.jsx
+++ gadget-lo/src/components/Header/Header.jsx
@@ -3,15 +3,23 @@ import "./Header.css";
 export const Header = () => {
   return (
     <div className="header flex-row space-between padding-10px-15px">
+      {/* Categories */}
+      <div className="menu-bar-web flex-row space-evenly gap-15px">
+        <p className="cursor link-style">Laptop</p>
+        <p className="cursor link-style">Mobile</p>
+        <p className="cursor link-style">Electronics</p>
+      </div>
+
       {/* Logo */}
-      <h2 className="cursor">Gadget Lo</h2>
+      <h2 className="cursor">LOGO</h2>
 
       {/* Menu bar */}
-      <div className="menu-bar-web flex-row space-evenly gap-10px">
-        <h3 className="cursor">Store</h3>
-        <h3 className="cursor">Cart</h3>
-        <h3 className="cursor">Wishlist</h3>
-        <h3 className="cursor">Login</h3>
+      <div className="menu-bar-web flex-row space-evenly gap-15px">
+        <p className="cursor link-style">Search</p>
+        <p className="cursor link-style">Store</p>
+        <p className="cursor link-style">Cart</p>
+        <p className="cursor link-style">Wishlist</p>
+        <p className="cursor link-style">Login</p>
       </div>
 
       {/* End */}
